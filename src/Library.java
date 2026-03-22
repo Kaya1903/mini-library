2133901e4917f1084d9cdace28fa926f147a984a
Kaya1903 <dku.doruk@gmail.com>
2026-03-22 15:44:51 +0100

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -18,16 +18,62 @@ public class Library {
     }
 
     public void printAvailableBooks() {
+        System.out.println("Available books:");
         for (int i = 0; i < bookCount; i++) {
-            books[i].printInfo();
+            if (books[i].isAvailable()) {
+                books[i].printInfo();
+            }
         }
+    }
+
+    public Book findBookByTitle(String title) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(title)) {
+                return books[i];
+            }
+        }
+        return null;
     }
 
     public int countAvailableBooks() {
         int count = 0;
         for (int i = 0; i < bookCount; i++) {
-            count++;
+            if (books[i].isAvailable()) {
+                count++;
+            }
         }
         return count;
+    }
+
+    public void borrowBook(String title, Reader reader) {
+        Book book = findBookByTitle(title);
+
+        if (book == null) {
+            System.out.println("Book not found: " + title);
+            return;
+        }
+
+        if (book.isAvailable()) {
+            book.borrow();
+            reader.increaseBorrowedCount();
+        } else {
+            System.out.println("Cannot borrow. Book already unavailable.");
+        }
+    }
+
+    public void returnBook(String title, Reader reader) {
+        Book book = findBookByTitle(title);
+
+        if (book == null) {
+            System.out.println("Book not found: " + title);
+            return;
+        }
+
+        if (!book.isAvailable()) {
+            book.returnBook();
+            reader.decreaseBorrowedCount();
+        } else {
+            System.out.println("This book is already in the library.");
+        }
     }
 }
