bdfcd26f86126388e83814cbaa7a394776d3ba32
Kaya1903 <dku.doruk@gmail.com>
2026-03-22 15:44:08 +0100

    Update Reader.java

--- src/Reader.java
+++ src/Reader.java
@@ -1,14 +1,30 @@
 public class Reader {
 
-    private String name;
-    private int readerId;
+    private String firstName;
+    private String lastName;
+    private int cardNumber;
+    private int borrowedCount;
 
-    public Reader(String name, int readerId) {
-        this.name = name;
-        this.readerId = readerId;
+    public Reader(String firstName, String lastName, int cardNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cardNumber = cardNumber;
+        this.borrowedCount = 0;
     }
 
-    public void printInfo() {
-        System.out.println("Reader: " + name + ", ID: " + readerId);
+    public void printData() {
+        System.out.println("Reader: " + firstName + " " + lastName
+                + ", card number: " + cardNumber
+                + ", borrowed count: " + borrowedCount);
+    }
+
+    public void increaseBorrowedCount() {
+        borrowedCount++;
+    }
+
+    public void decreaseBorrowedCount() {
+        if (borrowedCount > 0) {
+            borrowedCount--;
+        }
     }
 }
